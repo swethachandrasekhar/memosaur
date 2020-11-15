198570bd005b31e1fb353ffcd3936f67a69d58a2
Swetha Chandrasekhar <swethac68@gmail.com>
2020-11-15 13:35:08 -0500

    more clean up. Added logic for difficulty levels

--- scripts/script.js
+++ scripts/script.js
@@ -15,58 +15,170 @@ const dinosaurs = [
     name: "baryonyx",
     url: "./assets/baryonyx.jpg",
   },
-//   {
-//     name: "deinonychus",
-//     url: "./assets/deinonychus.jpg",
-//   },
-//   {
-//     name: "deinonychus",
-//     url: "./assets/deinonychus.jpg",
-//   },
-//   {
-//     name: "stegosaurus",
-//     url: "./assets/stegosaurus.jpg",
-//   },
-//   {
-//     name: "stegosaurus",
-//     url: "./assets/stegosaurus.jpg",
-//   },
-//   {
-//     name: "velociraptor",
-//     url: "./assets/velociraptor.jpg",
-//   },
-//   {
-//     name: "velociraptor",
-//     url: "./assets/velociraptor.jpg",
-//   },
-//   {
-//     name: "triceratops",
-//     url: "./assets/triceratops.jpg",
-//   },
-//   {
-//     name: "triceratops",
-//     url: "./assets/triceratops.jpg",
-//   },
-//   {
-//     name: "lambeosaurus",
-//     url: "./assets/lambeosaurus.jpg",
-//   },
-//   {
-//     name: "lambeosaurus",
-//     url: "./assets/lambeosaurus.jpg",
-//   },
-//   {
-//     name: "trex",
-//     url: "./assets/trex.jpg",
-//   },
-//   {
-//     name: "trex",
-//     url: "./assets/trex.jpg",
-//   },
+  {
+    name: "deinonychus",
+    url: "./assets/deinonychus.jpg",
+  },
+  {
+    name: "deinonychus",
+    url: "./assets/deinonychus.jpg",
+  },
+  {
+    name: "stegosaurus",
+    url: "./assets/stegosaurus.jpg",
+  },
+  {
+    name: "stegosaurus",
+    url: "./assets/stegosaurus.jpg",
+  },
+  {
+    name: "velociraptor",
+    url: "./assets/velociraptor.jpg",
+  },
+  {
+    name: "velociraptor",
+    url: "./assets/velociraptor.jpg",
+  },
+  {
+    name: "triceratops",
+    url: "./assets/triceratops.jpg",
+  },
+  {
+    name: "triceratops",
+    url: "./assets/triceratops.jpg",
+  },
+  {
+    name: "lambeosaurus",
+    url: "./assets/lambeosaurus.jpg",
+  },
+  {
+    name: "lambeosaurus",
+    url: "./assets/lambeosaurus.jpg",
+  },
+  {
+    name: "trex",
+    url: "./assets/trex.jpg",
+  },
+  {
+    name: "trex",
+    url: "./assets/trex.jpg",
+  },
 ];
 
-const myApp = {};
+const animals = [
+  {
+    name: "allosaurus",
+    url: "./assets/allosaurus.jpg",
+  },
+  {
+    name: "baryonyx",
+    url: "./assets/baryonyx.jpg",
+  },
+  {
+    name: "deinonychus",
+    url: "./assets/deinonychus.jpg",
+  },
+  {
+    name: "stegosaurus",
+    url: "./assets/stegosaurus.jpg",
+  },
+  {
+    name: "velociraptor",
+    url: "./assets/velociraptor.jpg",
+  },
+  {
+    name: "triceratops",
+    url: "./assets/triceratops.jpg",
+  },
 
+  {
+    name: "lambeosaurus",
+    url: "./assets/lambeosaurus.jpg",
+  },
+ {
+    name: "trex",
+    url: "./assets/trex.jpg",
+  },
+
+  {
+    name: "WoolyRhino",
+    url: "./assets/woolyrhino.png"
+  },
+  {
+    name: "anklyosaurus",
+    url: "./assets/Anklyosaurus.png",
+  },
+
+  {
+    name: "bueraptor",
+    url: "./assets/blueraptor.jpg",
+  },
+
+  {
+    name: "diabloceratops",
+    url: "./assets/Diabloceratops_The_Isle.png",
+  },
+
+  {
+    name: "dracorex",
+    url: "assets/Dracorex-_1.jpg",
+  },
+
+    {
+    name: "mastadon",
+      url: "./assets/mastadon.jpg",
+  },
+
+  {
+    name: "sabertooth",
+    url: "./assets/sabertooth.jpg",
+  },
+
+  {
+    name: "spinosaurus",
+    url: "./assets/Spinothumb.png",
+  },
+
+  {
+    name: "sinorinthosaurus",
+    url: "./assets/sinorinthosauraus.jpg",
+  },
+  {
+    name: "trodon",
+    url: "./assets/Trodon.png",
+  },
+  {
+    name: "lion",
+    url: "./assets/lion.jpeg",
+  },
+  {
+    name: "wolf",
+    url: "./assets/wolf.jpg",
+  },
+  {
+    name: "bear",
+    url: "./assets/bear.jpeg",
+  },
+  {
+    name: "croc",
+    url: "./assets/croc.jpg",
+  },
+  {
+    name: "owl",
+    url: "./assets/owl.jpg",
+  },
+  {
+    name: "panda",
+    url: "./assets/panda.jpg",
+  },
+  {
+    name: "chimp",
+    url: "./assets/chimp.jpg",
+  },
+
+
+]
+const myApp = {};
 myApp.moves = 0;
 myApp.firstMove = null;
 myApp.secondMove = null;
@@ -78,13 +190,38 @@ myApp.opened = [];
 myApp.moves = 0;
 myApp.timer;
 myApp.seconds = 0;
+myApp.finalAnimalArray = [];
+
+//Make a function to display appropriate 
+
+myApp.createCardsStack = (rows=4, columns=4) => {
+
+  myApp.gameRestart();
+
+  let numberOfUniqueCards = ((rows * columns) / 2);
+  let newAnimalArray = [];
+  myApp.finalAnimalArray = [];
+  newAnimalArray = animals.slice(0, numberOfUniqueCards);
+    newAnimalArray.forEach(animal => {
+    myApp.finalAnimalArray.push(animal);
+      myApp.finalAnimalArray.push(animal);
+  });
+  // myApp.newAnimalArray.push(myApp.finalAnimalArray)
+  console.log(`new array is `, myApp.finalAnimalArray);
+  myApp.displayCard() 
+
+}
+
 
 //This function loads the cards on page load
 myApp.displayCard = () => {
-  myApp.shuffle(dinosaurs);
+  // empty the grid
+  $(".grid").empty();
+console.log(`grid deets`, $(".grid").val());
+  myApp.shuffle(myApp.finalAnimalArray );
 
-  dinosaurs.forEach((dinos, index) => {
-    console.log(dinos, index);
+  myApp.finalAnimalArray.forEach((dinos, index) => {
+    // console.log(dinos, index);
     let cardsHtml = `
             <div class="card">
                     <div class="cardFace cardBack" data-id="${index}">
@@ -94,7 +231,7 @@ myApp.displayCard = () => {
                         <img  src="./assets/claws.png" alt="claws">
                     </div>
                 </div>`;
-
+    
     $(".grid").append(cardsHtml);
   });
 };
@@ -121,10 +258,12 @@ myApp.isGameOver = () => {};
 myApp.storeCard = (selectedCardContainer) => {
     console.log($(selectedCardContainer).children().eq(1).attr("class"));
   let cardSide = $(selectedCardContainer).children().eq(1).attr("class");
+  let cardContainerVisible = $(selectedCardContainer).attr("class");
+  console.log(`contianer visibe? ${cardContainerVisible}`);
 
   //check if the user is clicking on the same card
 
-  if (cardSide !== "cardFace cardBack") {
+  if (cardSide !== "cardFace cardBack" && cardContainerVisible !== 'card visible') {
     console.log('here')
     
     $(selectedCardContainer).toggleClass('visible');
@@ -134,7 +273,7 @@ myApp.storeCard = (selectedCardContainer) => {
     //QUESTION - should this be in a variable -> $(this).children().attr("data-id")
 
     openedCard.name =
-      dinosaurs[$(selectedCardContainer).children().eq(0).attr("data-id")].name;
+      myApp.finalAnimalArray[$(selectedCardContainer).children().eq(0).attr("data-id")].name;
     openedCard.id = $(selectedCardContainer).children().eq(0).attr("data-id");
     myApp.opened.push({ name: openedCard.name, id: openedCard.id });
     myApp.moves++;
@@ -157,7 +296,7 @@ myApp.compareCards = () => {
       });
 
       myApp.updateSuccessfulMoves();
-      if (myApp.resultArray.length === dinosaurs.length) {
+      if (myApp.resultArray.length === myApp.finalAnimalArray.length) {
         let totalTime = myApp.getTotalTime();
         let rating = myApp.getRatings();
         console.log(`total time ${totalTime} and rating is ${rating}`)
@@ -190,7 +329,7 @@ myApp.compareCards = () => {
         //   $(`[data-id="${card.id}"]`).siblings().addClass("visible");
         });
         myApp.opened = [];
-      }, 1200);
+      }, 1000);
     }
   }
 };
@@ -206,6 +345,7 @@ myApp.updateMoves = () => {
 myApp.startTimer = function () {
   //   let seconds = 0;
 
+  myApp.seconds = 0;
   function pad(val) {
     return val > 9 ? val : "0" + val;
   }
@@ -225,12 +365,12 @@ myApp.getTotalTime = () => {
 
 myApp.getRatings = () => {
     let ratings = 0;
-    if (myApp.moves === dinosaurs.length)
+  if (myApp.moves === myApp.finalAnimalArray.length)
     {
         return (ratings = 5);
-    }else if (myApp.moves === dinosaurs.length + (dinosaurs.length * 0.25)) {
+  } else if (myApp.moves === myApp.finalAnimalArray.length + (myApp.finalAnimalArray.length * 0.25)) {
         return (ratings = 4);
-    }else if (myApp.moves === dinosaurs.length + (dinosaurs.length * 0.50)) {
+  } else if (myApp.moves === myApp.finalAnimalArray.length + (myApp.finalAnimalArray.length * 0.50)) {
         return (ratings = 3);
     }
     else return (ratings = 2);
@@ -247,12 +387,13 @@ myApp.gameRestart = () => {
   myApp.resultArray = [];
   myApp.opened = [];
   myApp.moves = 0;
+  clearInterval(myApp.timer);
   console.log(`clear all 1${myApp.resultArray.length}`);
   console.log(`clear all  2${myApp.opened.length}`);
   console.log(`clear all 3${myApp.moves}`);
 
   console.log(`timer`, myApp.timer);
-  myApp.displayCard();
+  // myApp.displayCard();
 };
 
 myApp.cardClickEventListener = function () {
@@ -261,10 +402,51 @@ myApp.cardClickEventListener = function () {
     myApp.gameRestart();
   });
 
+  $('form').on('change','input', function() {
+    clearInterval(myApp.timer);
+    myApp.timer = 0;
+    
+    const diffultyLevel = $(this).attr('value');
+    if (diffultyLevel === 'easy') {
+      
+      $(".grid").css({ 
+        "grid-template-columns": "repeat(4, 100px)",
+        "grid-template-rows":"repeat(4, 100px)",
+        // "justify-content": "space-evenly"
+      });
+      myApp.createCardsStack(4, 4);
+    
+
+    }
+    else if (diffultyLevel === 'medium'){
+      $(".grid").css({
+        "grid-template-columns": "repeat(8,100px)", 
+        "grid-template-rows": "repeat(4, 100px)",
+        "justify-content": "center"
+        
+     });
+      myApp.createCardsStack(4, 8);
+      
+    }
+    else if (diffultyLevel === 'hard'){
+      $(".grid").css({
+        "grid-template-columns": "repeat(10, 85px)", 
+        "grid-template-rows": "repeat(5, 85px)",
+        "justify-content": "center"
+        
+      });
+      myApp.createCardsStack(5, 10)
+      
+    }
+   
+
+  })
+
   $(".grid").on("click", ".card", function () {
       console.log(this)
     // change this to on click of start
     if (myApp.moves === 0) {
+
       myApp.startTimer();
     }
     myApp.storeCard(this);
@@ -272,6 +454,7 @@ myApp.cardClickEventListener = function () {
 };
 
 myApp.init = () => {
+  myApp.createCardsStack(3,3);
   myApp.cardClickEventListener();
 };
 
